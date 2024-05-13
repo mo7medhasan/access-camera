77fb5ee23da1a7d178cc00179a8adfb28b498b37
m-hassan-sarri <87263653+mo7medhasan@users.noreply.github.com>
2024-05-13 22:54:36 +0300

    Create index.jsx

---
+++ components/UploadsFile/index.jsx
@@ -0,0 +1,34 @@
+import { Upload } from "lucide-react";
+import { useRef } from "react";
+
+export default function UploadsFile({ onChange, image }){
+
+
+    const imageRef = useRef(null);
+   
+    const HandleUpload = (e) => {
+      const file = e.target.files[0];
+   
+      onChange(file)
+    };
+  
+    return (
+      <button className="bg-white p-5 m-5 flex justify-center items-center rounded-full"
+        onClick={() => {
+          imageRef.current?.click();
+        }}
+     
+      >
+        <input
+          className="hidden"
+          type="file"
+          multiple={false}
+          ref={imageRef}
+          accept="image/*,video/*"
+          onChange={HandleUpload}
+        />
+        <Upload />
+      </button>
+    );
+ 
+}
