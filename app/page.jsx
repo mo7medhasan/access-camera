d5def578bb85c661b01c6f6edd703ad46762e9e9
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2024-05-13 11:02:05 +0300

    add change

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function page() {
   return (
     <div>
-    <Link href={"/camera"} className='text-6xl  text-center'>
+    <Link href={"/camera"} className='w-full text-6xl  text-center'>
     camera
     </Link>
     </div>
