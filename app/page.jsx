d7b4b924d44ae2b9ea4641f8a11c75db6737d9a0
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2024-05-13 11:05:02 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Link from 'next/link';
 export default function page() {
   return (
-    <div>
+    <div className='w-full h-screen flex justify-center items-center'>
     <Link href={"/camera"} className='w-full text-6xl  text-center'>
     camera
     </Link>
