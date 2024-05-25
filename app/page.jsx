6a706ed5f72bf592712d44373431d554dc084b91
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2024-05-25 23:10:18 +0300

    dsmds

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,11 @@ import Link from 'next/link';
 export default function page() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-    <Link href={"/camera"} className='w-full text-6xl  text-center'>
+  <Link href={"/camera/111/5456"} className='w-full text-6xl  text-center'>
     camera
-    </Link>
-    </div>
+    </Link>  
+   <Link href={"/cam-phone/111/5456"} className='w-full text-6xl  text-center'>
+    camera+v2
+    </Link> </div>
   )
 }
