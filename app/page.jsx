13aa13b000e4ef37b6f8d3dfa843728d0e34236f
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2024-05-26 06:33:14 +0300

    gap-10

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import CustomVideoPlayer from '@/components/VideoComponents';
 export default function page() {
   return (
-    <div className='w-full nim-h-[100vh]  flex flex-col justify-center items-center'>
+    <div className='w-full nim-h-[100vh] gap-10 flex flex-col justify-center items-center'>
   <Link href={"/camera/111/5456"} className='w-full text-6xl  text-center'>
     camera
     </Link>  
